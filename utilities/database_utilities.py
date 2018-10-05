b54f3c17f53137a37eaef262ae3de307de1b2890
Manuel Zabala <mzaba014@gmail.com>
2018-10-05 19:23:53 -0400

    Finished create_database() which executes the words.db ddl, generates the needed SQLite words DB

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,17 @@
-def create_database(database_path: str)
-    # TODO: generate the db
-    pass
+import sqlite3 as lite
 
 
-def save_words_to_database(database_path: str, word_list: list)
+def create_database(database_path: str):
+    connection = lite.connect(database_path)
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("drop table if exists words")
+        ddl = "CREATE TABLE words  ( word TEXT NOT NULL PRIMARY KEY, usage_count INT DEFAULT 1 NOT NULL  ); "
+        cursor.execute(ddl)
+        ddl = "CREATE UNIQUE INDEX words_word_uindex ON words (word);"
+        cursor.execute(ddl)
+
+
+def save_words_to_database(database_path: str, word_list: list):
     # TODO: save words to db
     pass
